d0becdcfcdce0b6f1f6b2fe57b49681f4b0d55ab
LuisCheats <luiselcrack183@gmail.com>
2026-02-02 23:15:52 -0500

    update

--- src/commands/tools-readviewonce.js
+++ src/commands/tools-readviewonce.js
@@ -29,6 +29,6 @@ conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}rep
 handler.help = ['ver']
 handler.tags = ['tools']
 handler.command = ['readviewonce', 'read', 'readvo']
-handler.premium = true
+handler.premium = false
 
 export default handler
